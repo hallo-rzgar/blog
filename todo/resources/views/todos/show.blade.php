8d897f431568f7d3d25841b1c026edf799731670
halop <halopolo99@gmail.com>
2021-10-16 23:31:20 +0300

    create

--- todo/resources/views/todos/show.blade.php
+++ todo/resources/views/todos/show.blade.php
@@ -1,6 +1,14 @@
 @extends('layout.app')
 @section('content')
-    <h1>hello</h1>
+    <a class="btn btn-default" href="/">Get Back</a>
+    <div class="card mt-2 ">
+        <h5 class="card-header  bg-secondary text-white">Todos</h5>
+        <div class="card-body">
+            <h1 class="card-title"><a class="text-decoration-none" href="todo/{{$todo->id}}" >{{$todo->text}}</a></h1>
+            <div class="btn btn-danger m-2">{{$todo->due}}</div>
+            <p class="card-body"> {{$todo->body}}</p>
+         </div>
+    </div>
 
 @endsection
 
