1840fdd6a80445547b42c3a9e180b5d58580462c
halop <halopolo99@gmail.com>
2021-10-18 16:25:42 +0300

    create

--- todo/resources/views/inc/navbar.blade.php
+++ todo/resources/views/inc/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">TodoList</a>
+    <a class="navbar-brand" href="/">TodoList</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item {{Request::is('/')? 'active' :''}}">
-          <a class="nav-link " aria-current="page" href="/">Home</a>
+        <li class="nav-item">
+          <a class="nav-link  {{Request::is('/')? 'active' :''}} " aria-current="page" href="/">Home</a>
         </li>
-        <li class="nav-item {{Request::is('/')? 'active' :''}}">
-          <a class="nav-link" href="todo/create">Create Todo</a>
+        <li class="nav-item ">
+          <a class="nav-link  {{Request::is('todo/create')? 'active' :''}}" href="/todo/create">Create Todo</a>
 
         </li>
 
--- todo/resources/views/todos/show.blade.php
+++ todo/resources/views/todos/show.blade.php
@@ -7,8 +7,22 @@
             <h1 class="card-title"><a class="text-decoration-none" href="todo/{{$todo->id}}" >{{$todo->text}}</a></h1>
             <h5 class="btn btn-danger  btn-sm ">{{$todo->due}}</h5>
             <p class="card-body"> {{$todo->body}}</p>
-            <a href="/todo/{{$todo->id}}/edit" class="btn btn-success">Edit</a>
-         </div>
+            <div class="form-inline">
+                <button class="btn btn-success " >
+                    <a class="text-decoration-none"  style="color: white" href="/todo/{{$todo->id}}/edit" >Edit</a>
+
+                </button>
+                <p  >
+                    {!! Form::open(['action' =>[ 'App\Http\Controllers\TodosController@destroy', $todo->id], 'method'=>'POST']) !!}
+                    {{ Form::hidden('_method','  DELETE') }}
+
+                    {{ Form::bsSubmit('Delete','Delete',['class'=>"btn btn-success mt-1"] ) }}
+                    {!! Form::close() !!}
+                </p>
+
+            </div>
+
+        </div>
     </div>
 
 @endsection
